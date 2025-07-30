8e1359e9b810e6888d29f8d86a009349fdc0bedb
Syamprasad1212 <syamofficial1212@gmail.com>
2025-07-30 11:44:52 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,16 @@
+class Construct
+{
+	Construct()
+	{
+		System.out.println("CONSTRUCTOR IS CALLED");
+	}
+
+}
+
+class Constructor
+{
+	public static void main(String[] args)
+	{
+		Construct s=new Construct();
+	}
+}
