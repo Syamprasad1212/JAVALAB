85633d461a0febd4319f2bea10aa3bd872d7d059
Syamprasad1212 <syamofficial1212@gmail.com>
2025-08-13 11:47:42 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,22 @@
+class Even
+{
+	public static void main(String []args)
+	{
+		int arr[]={1,2,3,4,5,6};
+		int even=0;
+		int odd=0;
+		for(int i:arr)
+		{
+		   if(i%2==0)
+		   {
+		    	even++;
+		   }
+		   else
+		   {
+		    	odd++;
+		   }
+		}   
+		   System.out.println("even:"+even+"odd:"+odd);
+		
+	}
+}
