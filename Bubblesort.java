301b24c6534f49901801834175e82e80aee6e6ff
Syamprasad1212 <syamofficial1212@gmail.com>
2025-08-20 10:29:56 +0530

    Create Bubblesort.java

---
+++ Bubblesort.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class Bubblesort{
+	public static void main(String[] args)
+	{
+		int[] arr={2,4,3,5,7,6};
+		for(int i=0;i<arr.length-1;i++)
+		  {
+		   for(int j=0;j<arr.length-i-1;j++)
+		    {
+			if(arr[j]>arr[j+1])
+			 {
+			     int temp=arr[j];
+			     arr[j]=arr[j+1];
+			     arr[j+1]=temp;
+			 }
+		    }
+		}
+			System.out.print("Sorted array is:");
+			for(int i:arr)
+			{
+				System.out.print(i);
+			}
+	}
+}
