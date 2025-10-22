f38b51afa360d408a11807e13c9d15947f8ac9b5
Syamprasad1212 <syamofficial1212@gmail.com>
2025-10-22 18:18:46 +0530

    Create Date.java

---
+++ JavaRecord/Date.java
@@ -0,0 +1,41 @@
+//program to demonstrate the features of java.time package
+import java.time.*;
+import java.time.format.*;
+import java.time.temporal.*;
+class TimeDemo
+{
+        public static void main(String[] args)
+        {
+                LocalDate date=LocalDate.now();
+                System.out.println(date);
+                LocalDateTime datetime=LocalDateTime.now();
+                System.out.println(datetime);
+                DateTimeFormatter formtime = DateTimeFormatter.ofPattern("dd/MM/yyyy,hh:mm:ss");
+                System.out.println(datetime.format(formtime));
+                LocalDate nextmonday=date.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+                System.out.println(nextmonday);
+                LocalDate previousmonday=date.with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+                System.out.println(previousmonday);
+                LocalDate daymonth=date.with(TemporalAdjusters.firstDayOfNextMonth());
+                System.out.println(daymonth);
+                DayOfWeek first=daymonth.getDayOfWeek();
+                System.out.println(first);
+                LocalDate lastdaymonth=date.with(TemporalAdjusters.lastDayOfMonth());
+                System.out.println(lastdaymonth);
+                DayOfWeek last=lastdaymonth.getDayOfWeek();
+                System.out.println(last);
+                
+        }
+}
+
+
+Output:
+2025-10-20
+2025-10-20T12:22:19.904082500
+20/10/2025,12:22:19
+2025-10-27
+2025-10-13
+2025-11-01
+SATURDAY
+2025-10-31
+FRIDAY
