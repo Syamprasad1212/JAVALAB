2a8fe169314ae40fd39e64bd92289f74932b178b
Syamprasad1212 <syamofficial1212@gmail.com>
2025-10-22 18:17:21 +0530

    Create Daemon.java

---
+++ JavaRecord/Daemon.java
@@ -0,0 +1,24 @@
+Program:
+class DaemonDemo extends Thread {
+    public void run() {
+        try { 
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Daemon running");
+                Thread.sleep(500); 
+            }
+        }
+        catch (InterruptedException e) { }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DaemonDemo d = new DaemonDemo();
+        d.setDaemon(true); // Daemon thread
+        d.start();
+        System.out.println("Main thread Exited");
+    } 
+}
+
+
+Output:
+Main thread Exited
+Daemon running
